258897735ddde4cdf498859916f2118f4cdbb499
Jason <jabrady42@gmail.com>
2019-11-17 15:08:14 -0600

    WIP client topic change

--- client/client.go
+++ client/client.go
@@ -10,32 +10,57 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
 	"time"
 
+	"github.com/cznic/mathutil"
 	"github.com/jbrady42/h2chat"
 	"github.com/marcusolsson/tui-go"
-	"github.com/r3labs/sse"
+	"github.com/jbrady42/sse"
 	"golang.org/x/net/http2"
 )
 
-const baseUrl = "https://localhost:8000"
-const postUrl = baseUrl + "/messages"
-const topicUrl = baseUrl + "/topics"
-
 var httpVersion = flag.Int("version", 2, "HTTP version")
 
-var httpTrans *http2.Transport
-
-type client struct {
-	eventClient *sse.Client
-	events      chan *sse.Event
-	ui          tui.UI
-	history     *tui.Box
-}
+var logger *log.Logger
 
 func init() {
-	httpTrans = &http2.Transport{
+	f, err := os.Create("debug.log")
+	if err != nil {
+		// ...
+	}
+	// defer f.Close()
+
+	logger = log.New(f, "", log.LstdFlags)
+}
+
+type client struct {
+	baseUrl      string
+	httpTrans    *http2.Transport
+	eventClient  *sse.Client
+	Topics       []string
+	currentTopic string
+	eventChan    chan *sse.Event
+	ui           *chatUI
+}
+
+type chatUI struct {
+	ui      tui.UI
+	history *tui.Box
+}
+
+func NewClient(baseUrl string) client {
+	httpTrans := &http2.Transport{
 		TLSClientConfig: tlsConfig(),
+	}
+
+	eventClient := sse.NewClient(baseUrl + "/events")
+	eventClient.Connection.Transport = httpTrans
+
+	var topics []string
+	return client{
+		baseUrl, httpTrans, eventClient,
+		topics, "", nil, nil,
 	}
 }
 
@@ -57,18 +82,19 @@ func tlsConfig() *tls.Config {
 	return tlsConfig
 }
 
-func SendMessage(msg string) {
+func (t *client) SendMessage(msg string) {
+	postUrl := t.baseUrl + "/messages"
 	reqBody, err := json.Marshal(h2chat.Message{
 		Name:    "Test name",
 		Message: msg,
 		Time:    time.Now(),
-		Topic:   "default",
+		Topic:   t.currentTopic,
 	})
 	if err != nil {
 		log.Fatalf("Error encoding message %s", err)
 	}
 
-	client := getClient()
+	client := t.getClient()
 	resp, err := client.Post(postUrl, "application/json", bytes.NewBuffer(reqBody))
 	if err != nil {
 		log.Fatalf("Error posting message %s", err)
@@ -81,8 +107,13 @@ func SendMessage(msg string) {
 	// log.Printf("Response is %s", string(body))
 }
 
-func getTopics() []string {
-	client := getClient()
+func (t *client) loadTopics() {
+	t.Topics = t.getTopics()
+}
+
+func (t *client) getTopics() []string {
+	topicUrl := t.baseUrl + "/topics"
+	client := t.getClient()
 	resp, err := client.Get(topicUrl)
 	if err != nil {
 		log.Fatalf("Error fetching channels %s", err)
@@ -93,7 +124,7 @@ func getTopics() []string {
 	if err != nil {
 		log.Fatalf("Failed reading response body: %s", err)
 	}
-	log.Printf("Response is %s", string(body))
+	logger.Printf("Response is %s", string(body))
 
 	var topics []string
 	err = json.Unmarshal(body, &topics)
@@ -103,85 +134,90 @@ func getTopics() []string {
 	return topics
 }
 
-func getClient() *http.Client {
+func (t *client) getClient() *http.Client {
 	client := &http.Client{}
-	client.Transport = httpTrans
+	client.Transport = t.httpTrans
 	return client
-}
-
-// Max returns the larger of x or y.
-func Max(x, y int) int {
-	if x < y {
-		return y
-	}
-	return x
-}
-
-// Min returns the smaller of x or y.
-func Min(x, y int) int {
-	if x > y {
-		return y
-	}
-	return x
 }
 
 func setSelected(inc int, list *tui.List) {
 	current := list.Selected()
 	if inc < 0 {
-		current = Max(current+inc, 0)
+		current = mathutil.Max(current+inc, 0)
 	} else {
-		current = Min(current+inc, list.Length()-1)
+		current = mathutil.Min(current+inc, list.Length()-1)
 	}
 	list.Select(current)
 }
 
 func (t *client) subscribeTopic(topic string) {
+
+	logger.Println("Setting topic to " + topic)
+
+	// Unsubscribe first to prevent further UI updates
+	if t.eventChan != nil {
+		func(c chan *sse.Event) {
+			logger.Println("Start unsubscribe from old topic " + topic)
+			t.eventClient.Unsubscribe(c)
+			// close(c)
+		}(t.eventChan)
+		logger.Println("Unsubscribed from old topic " + topic)
+		t.eventChan = nil
+	}
+
+	if topic == "default" {
+		return
+	}
+	t.currentTopic = topic
 	events := make(chan *sse.Event)
 	err := t.eventClient.SubscribeChan(topic, events)
 	if err != nil {
 		log.Fatalf("Cant create listener %s", err)
 	}
-	t.events = events
-
+	t.eventChan = events
+	logger.Println("Subscribed to new topic " + topic)
 	go t.handleMessages(events)
 }
 
 func (t *client) handleMessages(events chan *sse.Event) {
 	for msg := range events {
+		logger.Println("Got message " + string(msg.Data))
 		var post h2chat.Message
 		err := json.Unmarshal(msg.Data, &post)
 		if err != nil {
 			log.Fatalf("Failed decoding incoming message %v", err)
 		}
-
-		t.ui.Update(func() {
-			t.history.Append(tui.NewHBox(
-				tui.NewLabel(post.Time.Format(time.Kitchen)),
-				tui.NewPadder(1, 0, tui.NewLabel(fmt.Sprintf("<%s>", post.Name))),
-				tui.NewLabel(post.Message),
-				tui.NewSpacer(),
-			))
-		})
+		t.ui.updateMessages(post)
 	}
+}
+
+func (t *chatUI) updateMessages(post h2chat.Message) {
+	t.ui.Update(func() {
+		t.history.Append(tui.NewHBox(
+			tui.NewLabel(post.Time.Format(time.Kitchen)),
+			tui.NewPadder(1, 0, tui.NewLabel(fmt.Sprintf("<%s>", post.Name))),
+			tui.NewLabel(post.Message),
+			tui.NewSpacer(),
+		))
+	})
+	// t.ui.Update(func() {})
 }
 
 func main() {
 	flag.Parse()
 
-	eventClient := sse.NewClient(baseUrl + "/events")
-	eventClient.Connection.Transport = httpTrans
+	baseUrl := "https://localhost:8000"
 
-	topics := getTopics()
+	client := NewClient(baseUrl)
+	client.loadTopics()
 
 	// GUI
 
 	tList := tui.NewList()
 
-	for _, a := range topics {
+	for _, a := range client.Topics {
 		tList.AddItems(a)
 	}
-
-	tList.Select(0)
 
 	sidebar := tui.NewVBox(
 		tList,
@@ -214,8 +250,14 @@ func main() {
 		if e.Text() == "" {
 			return // Skip empty messages
 		}
-		SendMessage(e.Text())
+		client.SendMessage(e.Text())
 		input.SetText("")
+	})
+
+	tList.OnSelectionChanged(func(l *tui.List) {
+		indx := l.Selected()
+		topic := client.Topics[indx]
+		go client.subscribeTopic(topic)
 	})
 
 	ui, err := tui.New(root)
@@ -230,10 +272,24 @@ func main() {
 	ui.SetKeybinding("PgUp", func() { setSelected(-1, tList) })
 	ui.SetKeybinding("PgDn", func() { setSelected(1, tList) })
 
-	client := client{eventClient, nil, ui, history}
+	cUI := &chatUI{ui, history}
+
+	client.ui = cUI
 
 	// Not blocking
-	client.subscribeTopic("default")
+	// client.subscribeTopic("default")
+	go func() {
+		time.Sleep(1 * time.Second)
+		ui.Update(func() {
+			tList.Select(1)
+		})
+		//
+		// time.Sleep(30 * time.Second)
+		// client.eventClient.Unsubscribe(client.eventChan)
+		//
+		// time.Sleep(30 * time.Second)
+		// client.subscribeTopic(client.currentTopic)
+	}()
 
 	if err := ui.Run(); err != nil {
 		log.Fatal(err)
--- go.mod
+++ go.mod
@@ -4,8 +4,11 @@ go 1.13
 
 require (
 	github.com/andrewburian/eventsource v2.1.0+incompatible
+	github.com/cznic/mathutil v0.0.0-20181122101859-297441e03548
+	github.com/jbrady42/sse v0.0.0-20191018093120-b8132ebb4c21
 	github.com/marcusolsson/tui-go v0.4.0
 	github.com/r3labs/sse v0.0.0-20191018093120-b8132ebb4c21
+	github.com/remyoudompheng/bigfft v0.0.0-20190728182440-6a916e37a237 // indirect
 	golang.org/x/net v0.0.0-20191116160921-f9c825593386
 	gopkg.in/cenkalti/backoff.v1 v1.1.0 // indirect
 )
